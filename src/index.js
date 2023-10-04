4f346c4e61fe8b1ff0c1d17ca978ebff406d56f2
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-04 03:59:00 +0300

    parse cli args

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const opts_1 = __importDefault(require("./opts"));
+console.log((0, opts_1.default)());
